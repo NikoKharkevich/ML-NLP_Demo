520497f4a9e48bbf4f556e6b844ad58775f9e38e
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-01-11 22:02:58 +0200

    Initial Commit

---
+++ ML-NLP_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML-NLP_Demo
+//
+//  Created by Nikola Kharkevich on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
