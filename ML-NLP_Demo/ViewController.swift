863505b11c9de6b11997b75cdb16753c1e312c8c
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-01-11 23:26:21 +0200

    added model and Swifter

--- ML-NLP_Demo/ViewController.swift
+++ ML-NLP_Demo/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  ML-NLP_Demo
-//
-//  Created by Nikola Kharkevich on 11.01.2022.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func predictPressed(_ sender: Any) {
+    
+    
+    }
+    
 }
 
