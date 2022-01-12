d460164ca5383b2f6e1bdcd610e8e3bdd598101e
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-01-12 13:17:17 +0200

    testing gitignore

--- ML-NLP_Demo/ViewController.swift
+++ ML-NLP_Demo/ViewController.swift
@@ -1,13 +1,17 @@
 
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    let swifter = Swifter(consumerKey: "1481010055491334146-kpkH6T1zP8ensfVORcZ43GTztZ28sV",
+                      consumerSecret: "n1kshv5p2uLfHAOBQve8pRZU6uTJSpa0m4Ea8UV4zUqzk")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
